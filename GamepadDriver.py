a9a8a6218b7ffa43dc13d96d872d428551e1053a
BeckSM64 <becksm64@gmail.com>
2024-01-30 15:03:55 -0500

    :sparkles: add dpad and conversion function for analog stick values

--- GamepadDriver.py
+++ GamepadDriver.py
@@ -13,6 +13,10 @@ AButton = 15
 BButton = 16
 XButton = 24
 YButton = 23
+DPadUpButton = 5
+DPadDownButton = 6
+DPadLeftButton = 26
+DPadRightButton = 12
 
 # Setup GPIO Mode
 GPIO.setmode(GPIO.BCM)
@@ -21,6 +25,10 @@ GPIO.setup(AButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # a button
 GPIO.setup(BButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # b button
 GPIO.setup(XButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # x button
 GPIO.setup(YButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # y button
+GPIO.setup(DPadUpButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpadup button
+GPIO.setup(DPadDownButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpaddown button
+GPIO.setup(DPadLeftButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpadleft button
+GPIO.setup(DPadRightButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpadright button
 
 # Creating the virtual gamepad
 events = (
@@ -63,11 +71,32 @@ def analogRead(chn):
 def analogWrite(value):
     bus.write_byte_data(address,cmd,value)
 
+def convertValue(oldValue):
+
+    # Convert the value
+    if oldValue < 128:
+        oldRange = (127 - -57)
+        newRange = (127 - 0)
+        newValue = (((oldValue - -57) * newRange) / oldRange) + 0
+    elif oldValue > 128:
+        oldRange = (175 - 129)
+        newRange = (255 - 129)
+        newValue = (((oldValue - 129) * newRange) / oldRange) + 129
+    else:
+        return oldValue
+
+    # Check if value is negative, then force zero
+    if newValue < 0:
+        newValue = 0
+
+    return newValue
+
 try:
     while True:
         val_Z = GPIO.input(Z_Pin)
         val_Y = analogRead(0)
         val_X = analogRead(2)
+        val_X = convertValue(val_X - 79)
         val_RY = analogRead(1)
         val_RX = analogRead(3)
         print("Click: %d, Y: %d, X: %d" % (val_Z, val_Y, val_X))
@@ -101,6 +130,31 @@ try:
             device.emit(uinput.BTN_Y, 1)
         else:
             device.emit(uinput.BTN_Y, 0)
+
+        if GPIO.input(DPadUpButton) == GPIO.LOW:
+            print("D-Pad Up Button Pressed")
+            device.emit(uinput.BTN_DPAD_UP, 1)
+        else:
+            device.emit(uinput.BTN_DPAD_UP, 0)
+
+        if GPIO.input(DPadDownButton) == GPIO.LOW:
+            print("D-Pad Down Button Pressed")
+            device.emit(uinput.BTN_DPAD_DOWN, 1)
+        else:
+            device.emit(uinput.BTN_DPAD_DOWN, 0)
+
+        if GPIO.input(DPadLeftButton) == GPIO.LOW:
+            print("D-Pad Left Button Pressed")
+            device.emit(uinput.BTN_DPAD_LEFT, 1)
+        else:
+            device.emit(uinput.BTN_DPAD_LEFT, 0)
+
+        if GPIO.input(DPadRightButton) == GPIO.LOW:
+            print("D-Pad Right Button Pressed")
+            device.emit(uinput.BTN_DPAD_RIGHT, 1)
+        else:
+            device.emit(uinput.BTN_DPAD_RIGHT, 0)
+
 except:
     GPIO.cleanup()
     device.destroy()
