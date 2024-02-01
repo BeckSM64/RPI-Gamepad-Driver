f9a9172de2c05f0496d15122a42567bd0ea1f8c7
BeckSM64 <becksm64@gmail.com>
2024-02-01 18:00:34 -0500

    :sparkles: add remaining buttons

--- GamepadDriver.py
+++ GamepadDriver.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import smbus
 import uinput
+import subprocess
 
 address = 0x48
 bus=smbus.SMBus(1)
@@ -17,6 +18,16 @@ DPadUpButton = 5
 DPadDownButton = 6
 DPadLeftButton = 26
 DPadRightButton = 12
+StartButton = 20
+SelectButton = 21
+VolumeUpButton = 13
+VolumeDownButton = 19
+L2Button = 14
+L1Button = 18
+R2Button = 8
+R1Button = 7
+L3Button = 25
+R3Button = 17
 
 # Setup GPIO Mode
 GPIO.setmode(GPIO.BCM)
@@ -29,6 +40,16 @@ GPIO.setup(DPadUpButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpadup button
 GPIO.setup(DPadDownButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpaddown button
 GPIO.setup(DPadLeftButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpadleft button
 GPIO.setup(DPadRightButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # dpadright button
+GPIO.setup(StartButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # start button button
+GPIO.setup(SelectButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # select button
+GPIO.setup(VolumeUpButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # volume up button
+GPIO.setup(VolumeDownButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # volume down button
+GPIO.setup(L2Button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # l2 button
+GPIO.setup(L1Button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # l1 button
+GPIO.setup(R2Button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # r2 button
+GPIO.setup(R1Button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # r1 button
+GPIO.setup(L3Button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # l3 button
+GPIO.setup(R3Button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # r3 button
 
 # Creating the virtual gamepad
 events = (
@@ -46,6 +67,8 @@ events = (
 	uinput.BTN_TL2,
 	uinput.BTN_TR,
 	uinput.BTN_TR2,
+    uinput.BTN_THUMBL,
+    uinput.BTN_THUMBR,
     uinput.ABS_X + (0, 255, 0, 0),
     uinput.ABS_Y + (0, 255, 0, 0),
     uinput.ABS_RX + (0, 255, 0, 0),
@@ -154,6 +177,62 @@ try:
             device.emit(uinput.BTN_DPAD_RIGHT, 1)
         else:
             device.emit(uinput.BTN_DPAD_RIGHT, 0)
+
+        if GPIO.input(StartButton) == GPIO.LOW:
+            print("Start Button Pressed")
+            device.emit(uinput.BTN_START, 1)
+        else:
+            device.emit(uinput.BTN_START, 0)
+
+        if GPIO.input(SelectButton) == GPIO.LOW:
+            print("Select Button Pressed")
+            device.emit(uinput.BTN_SELECT, 1)
+        else:
+            device.emit(uinput.BTN_SELECT, 0)
+
+        if GPIO.input(VolumeUpButton) == GPIO.LOW:
+            print("Volume Up Button Pressed")
+            subprocess.run(["amixer", "-q", "-M", "sset", "Headphone", "5%+"])
+
+        if GPIO.input(VolumeDownButton) == GPIO.LOW:
+            print("Volume Down Button Pressed")
+            subprocess.run(["amixer", "-q", "-M", "sset", "Headphone", "5%-"])
+
+        if GPIO.input(L2Button) == GPIO.LOW:
+            print("L2 Button Pressed")
+            device.emit(uinput.BTN_TL2, 1)
+        else:
+            device.emit(uinput.BTN_TL2, 0)
+
+        if GPIO.input(L1Button) == GPIO.LOW:
+            print("L1 Button Pressed")
+            device.emit(uinput.BTN_TL, 1)
+        else:
+            device.emit(uinput.BTN_TL, 0)
+
+        if GPIO.input(R2Button) == GPIO.LOW:
+            print("R2 Button Pressed")
+            device.emit(uinput.BTN_TR2, 1)
+        else:
+            device.emit(uinput.BTN_TR2, 0)
+
+        if GPIO.input(R1Button) == GPIO.LOW:
+            print("R1 Button Pressed")
+            device.emit(uinput.BTN_TR, 1)
+        else:
+            device.emit(uinput.BTN_TR, 0)
+
+        if GPIO.input(L3Button) == GPIO.LOW:
+            print("L3 Button Pressed")
+            device.emit(uinput.BTN_THUMBL, 1)
+        else:
+            device.emit(uinput.BTN_THUMBL, 0)
+
+        if GPIO.input(R3Button) == GPIO.LOW:
+            print("R3 Button Pressed")
+            device.emit(uinput.BTN_THUMBR, 1)
+        else:
+            device.emit(uinput.BTN_THUMBR, 0)
 
 except:
     GPIO.cleanup()
